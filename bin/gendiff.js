c3095d29a3d632d3f3674cef345fcf7db3090474
smlnx <alexander.is.dev@gmail.com>
2020-11-09 23:28:24 +0300

    Specify(bin/gendiff.js) required command-arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,7 @@
 import commander from 'commander';
 
 commander
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0');
 
