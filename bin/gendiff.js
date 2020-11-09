72a7f323facb844ec9c049cd0a5945dbfe60180c
smlnx <alexander.is.dev@gmail.com>
2020-11-09 23:37:32 +0300

    Specify(bin/gendiff.js) options info

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,6 @@ commander
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
 
 commander.parse(process.argv);
