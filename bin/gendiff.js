f7100fec5669eb9ecb8b6c1784b752bb1120fe13
smlnx <alexander.is.dev@gmail.com>
2020-11-09 23:34:41 +0300

    Delete(bin/gendiff.js) semicolon after version function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,6 @@ import commander from 'commander';
 commander
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
 
 commander.parse(process.argv);
