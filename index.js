b3722a47050c08668e913d5421f6681736727a2c
smlnx <alexander.is.dev@gmail.com>
2020-12-03 00:54:39 +0300

    Refactor(index.js) getting of full file path

--- index.js
+++ index.js
@@ -2,16 +2,12 @@
 
 import differ from './src/differ.js';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default (file1Path, file2Path) => {
   // TODO: Кому делигоровать обработку относительных и абсолютных путей?
   // оставить ее в index.js или перенести в src/differ.js?
-  const fullPath1 = path.resolve(__dirname, file1Path);
-  const fullPath2 = path.resolve(__dirname, file2Path);
+  const fullPath1 = path.resolve(process.cwd(), file1Path);
+  const fullPath2 = path.resolve(process.cwd(), file2Path);
 
   return differ(fullPath1, fullPath2);
 };
