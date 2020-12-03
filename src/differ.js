293bc663028380d6cec946f1302d688082a9aa6f
smlnx <alexander.is.dev@gmail.com>
2020-12-03 22:40:24 +0300

    Refactor(src/differ.js) getting of allUniqKeys in calculateDifference function

--- src/differ.js
+++ src/differ.js
@@ -14,12 +14,15 @@ const calculateDifference = (obj1, obj2) => {
   const obj2Keys = Object.keys(obj2);
   const allKeys = [...obj1Keys, ...obj2Keys];
   /* Или вместо reduce использ. метод из lodash uniq() */
-  const allUniqKeys = allKeys
-    .reduce((acc, key) => (
-      acc.includes(key)
-        ? acc
-        : [...acc, key]
-    ), []);
+  // const allUniqKeys = allKeys
+  //   .reduce((acc, key) => (
+  //     acc.includes(key)
+  //       ? acc
+  //       : [...acc, key]
+  //   ), []);
+
+  const allKeysSet = new Set(allKeys);
+  const allUniqKeys = Array.from(allKeysSet);
 
   allUniqKeys.sort((a, b) => a.localeCompare(b, 'en'));
 
