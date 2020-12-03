c6b8fe14e306b8db523cc626e9ae5a366fa048a1
smlnx <alexander.is.dev@gmail.com>
2020-12-03 19:42:21 +0300

    Clear(src/differ.js) fix eslint error

--- src/differ.js
+++ src/differ.js
@@ -8,29 +8,31 @@ const readFile = (filePath) => {
   return data;
 };
 
-/* Вариант на строках */
-const calculateDifference = (data1, data2) => {
-  const data1Keys = Object.keys(data1);
-  const data2Keys = Object.keys(data2);
-  const datasKeys = [...data1Keys, ...data2Keys];
+const calculateDifference = (obj1, obj2) => {
+  const obj1Keys = Object.keys(obj1);
+  const obj2Keys = Object.keys(obj2);
+  const allKeys = [...obj1Keys, ...obj2Keys].sort((a, b) => a.localeCompare(b, 'en'));
+  /* Или вместо reduce использ. метод из lodash uniq() */
+  const uniqAllKeys = allKeys.reduce((acc, key) => (
+    acc.includes(key)
+      ? acc
+      : [...acc, key]
+  ),
+  []);
 
-  const datasKeysSet = new Set(datasKeys);
-  const uniqDatasKeys = Array.from(datasKeysSet).sort((a, b) => a.localeCompare(b, 'en'));
-
-  const rows = uniqDatasKeys.reduce((acc, key) => {
-    if (data1Keys.includes(key) && data2Keys.includes(key)) {
-      return data1[key] === data2[key]
-        ? [...acc, `    ${key}: ${data1[key]}`]
-        : [...acc, `  - ${key}: ${data1[key]}`, `  + ${key}: ${data2[key]}`];
+  const rows = uniqAllKeys.reduce((acc, key) => {
+    if (obj1[key] && obj2[key]) {
+      return obj1[key] === obj2[key]
+        ? [...acc, `    ${key}: ${obj1[key]}`]
+        : [...acc, `  - ${key}: ${obj1[key]}`, `  + ${key}: ${obj2[key]}`];
     }
-    if (data1Keys.includes(key)) {
-      return [...acc, `  - ${key}: ${data1[key]}`];
+    if (!obj2[key]) {
+      return [...acc, `  - ${key}: ${obj1[key]}`];
     }
-    return [...acc, `  + ${key}: ${data2[key]}`];
+    return [...acc, `  + ${key}: ${obj2[key]}`];
   }, []).join('\n');
 
   const res = `{\n${rows}\n}`;
-
   return res;
 };
 
