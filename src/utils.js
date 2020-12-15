51ad6c6f62d63d9badffb97f1545328942d492ea
smlnx <alexander.is.dev@gmail.com>
2020-12-15 20:25:41 +0300

    Add(src/) utils.js

---
+++ src/utils.js
@@ -0,0 +1,14 @@
+export const isObject = (value) => (
+  value === null
+    ? false
+    : value.constructor.name === 'Object'
+);
+
+export const getKeysUnion = (obj1, obj2) => {
+  const obj1Keys = Object.keys(obj1);
+  const obj2Keys = Object.keys(obj2);
+  const allKeys = [...obj1Keys, ...obj2Keys];
+  const allKeysSet = new Set(allKeys);
+
+  return Array.from(allKeysSet);
+};
