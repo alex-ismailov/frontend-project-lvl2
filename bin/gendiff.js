d63359e38e306da5364195e11bea71f3eff91192
smlnx <alexander.is.dev@gmail.com>
2020-12-15 19:30:49 +0300

    Refactor(bin/gendiff.js) add output format option processing

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ commander
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2) => console.log(app(filepath1, filepath2)));
+  .option('-f, --format [type]', 'output format (default: "stylish")', 'stylish')
+  .action((filepath1, filepath2) => console.log(app(filepath1, filepath2, commander.format)));
 
 commander.parse(argv);
