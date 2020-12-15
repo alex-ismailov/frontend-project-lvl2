17d7f3999ac486de0efad5ffd1d03831401c80bd
smlnx <alexander.is.dev@gmail.com>
2020-12-15 21:47:20 +0300

    Remove(src/formatter.js) comment

--- src/formatter.js
+++ src/formatter.js
@@ -9,16 +9,3 @@ export default (type = 'stylish') => (ast) => {
       throw new Error(`Unknown formatter type: ! ---> ${type} <--- !`);
   }
 };
-
-// Это все красиво, конечно, но если передать не корректный
-// аргумент type, то в stackTrace попадет инфа о следствиях ошибки, а
-// не о ее причинах.
-// Поэтому буду использовать switch
-// const formattersMap = {
-//   stylish: (ast) => stylish(ast),
-//   stringStylish: (ast) => stringStylish(ast),
-// };
-
-// export default (type = 'stylish') => (ast) => (
-//   formattersMap[type](ast)
-// );
