b166727c495c5d7c80b915095d7fb8f15058541d
smlnx <alexander.is.dev@gmail.com>
2020-12-17 03:17:44 +0300

    Add(src/formatters/index.js) plain case

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,12 @@
 import stylish from './stylish.js';
+import plain from './plain.js';
 
 export default (type = 'stylish') => (ast) => {
   switch (type) {
     case 'stylish':
       return stylish(ast);
+    case 'plain':
+      return plain(ast);
 
     default:
       throw new Error(`Unknown formatter type: ! ---> ${type} <--- !`);
