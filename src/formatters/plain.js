cbf88831a5539e08434f424b65cc309c15c88bb7
smlnx <alexander.is.dev@gmail.com>
2020-12-17 03:23:25 +0300

    Implement(src/formatters/) plain.js formatter

---
+++ src/formatters/plain.js
@@ -0,0 +1,52 @@
+import { isObject } from '../utils';
+
+const actionsMap = {
+  added: 'was added with value:',
+  removed: 'was removed',
+  updated: 'was updated.',
+};
+
+const stringTypesMap = {
+  added: (path, type, value) => `Property '${path}' ${actionsMap[type]} ${value}`,
+  removed: (path, type) => `Property '${path}' ${actionsMap[type]}`,
+  updated: (path, type, prevValue, value) => `Property '${path}' ${actionsMap[type]} From ${prevValue} to ${value}`,
+};
+
+const normalizeValue = (value) => {
+  if (isObject(value)) {
+    return '[complex value]';
+  }
+  return typeof value === 'string'
+    ? `'${value}'`
+    : value;
+};
+
+const makeString = (path, type, value, prevValue) => (
+  type === 'updated'
+    ? stringTypesMap[type](path, type, normalizeValue(prevValue), normalizeValue(value))
+    : stringTypesMap[type](path, type, normalizeValue(value))
+);
+
+const stringsMap = {
+  updated: (keyNode, path) => makeString(path, keyNode.type, keyNode.value, keyNode.prevValue),
+  added: (keyNode, path) => makeString(path, keyNode.type, keyNode.value),
+  removed: (keyNode, path) => makeString(path, keyNode.type, keyNode.value),
+};
+
+export default (ast) => {
+  const formatterIter = (currentAst, prevPath) => currentAst
+    .reduce((acc, keyNode) => {
+      const { type, name } = keyNode;
+      const currentPath = prevPath === null ? name : `${prevPath}.${name}`;
+      if (type === 'same') {
+        return acc;
+      }
+
+      return type === 'parent'
+        ? [...acc, formatterIter(keyNode.children, currentPath)]
+        : [...acc, stringsMap[type](keyNode, currentPath)];
+    }, [])
+    .join('\n');
+
+  return formatterIter(ast, null);
+};
