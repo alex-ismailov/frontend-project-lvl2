40c21d4e16a036df30213ac0c1152d6bbf5069d4
smlnx <alexander.is.dev@gmail.com>
2020-12-19 02:10:27 +0300

    Fix(bin/gendiff.js) default output format, remove dublicates in bash output

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ commander
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format [type]', 'output format (default: "stylish")', 'stylish')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => console.log(app(filepath1, filepath2, commander.format)));
 
 commander.parse(argv);
