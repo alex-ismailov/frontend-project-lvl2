c1ee49305126cca73e1dbec499db9cb93524aaec
smlnx <alexander.is.dev@gmail.com>
2020-12-18 16:22:09 +0300

    Add(src/formatters/index.js) json formatter case

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,12 +1,15 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js'
 
-export default (type = 'stylish') => (ast) => {
+export default (type = 'stylish') => (ast, firstFilePath, secondFilePath) => {
   switch (type) {
     case 'stylish':
       return stylish(ast);
     case 'plain':
       return plain(ast);
+    case 'json':
+        return json(ast, firstFilePath, secondFilePath)
 
     default:
       throw new Error(`Unknown formatter type: ! ---> ${type} <--- !`);
