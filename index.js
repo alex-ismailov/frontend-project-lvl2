b37d78c8c97c3792f9605f0d996b36e30ddba604
smlnx <alexander.is.dev@gmail.com>
2020-12-19 01:56:39 +0300

    Refactor(index.js) for new report structure

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-/* eslint-disable import/order */
 import path from 'path';
 import { readFile } from './src/utils.js';
 import parsers from './src/parsers.js';
 import getObjectsDiffAST from './src/differ.js';
 import getFormatter from './src/formatters/index.js';
 
-export default (file1Path, file2Path, outputFormat = 'stylish') => {
+export default (file1Path, file2Path, formatStyle = 'stylish') => {
   const fullPath1 = path.resolve(process.cwd(), file1Path);
   const fullPath2 = path.resolve(process.cwd(), file2Path);
 
@@ -19,9 +18,7 @@ export default (file1Path, file2Path, outputFormat = 'stylish') => {
   const data2 = parsers[file2Extension](rawData2);
 
   const ast = getObjectsDiffAST(data1, data2);
-  const formatter = getFormatter(outputFormat);
+  const formatter = getFormatter(formatStyle);
 
-  return outputFormat === 'json'
-    ? formatter(ast, fullPath1, fullPath2)
-    : formatter(ast);
+  return formatter(ast);
 };
