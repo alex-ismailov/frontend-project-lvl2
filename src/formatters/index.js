f861813a30c4aa5a2b461ac1bb121550dde3aa69
smlnx <alexander.is.dev@gmail.com>
2020-12-19 01:56:10 +0300

    Refactor(src/formatters/index.js) for new report structure

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,14 +2,14 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-export default (type = 'stylish') => (ast, firstFilePath, secondFilePath) => {
+export default (type = 'stylish') => (ast) => {
   switch (type) {
     case 'stylish':
       return stylish(ast);
     case 'plain':
       return plain(ast);
     case 'json':
-      return json(ast, firstFilePath, secondFilePath);
+      return json(ast);
 
     default:
       throw new Error(`Unknown formatter type: ! ---> ${type} <--- !`);
