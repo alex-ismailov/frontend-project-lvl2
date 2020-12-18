7b04b5aa1c1b40cb5bf93e2e84c1c50419f887cb
smilenb <alexander.is.dev@gmail.com>
2020-12-18 20:48:02 +0300

    Implement(src/formatters/) json.js formatter - tests pass

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,9 +1,9 @@
-const makeMessageItem = (actionId, message, path, value, prevValue) => {
+const makeMessageItem = (actionId, message, propPath, value, prevValue) => {
   if (actionId === 'updated') {
     return {
       actionId,
       message,
-      path,
+      propPath,
       value,
       prevValue,
     };
@@ -12,72 +12,64 @@ const makeMessageItem = (actionId, message, path, value, prevValue) => {
   return {
     actionId,
     message,
-    path,
+    propPath,
     value,
   };
 };
 
-const messageTextsMap = {
+const textsMap = {
   updated: 'Property was updated',
   added: 'Property was added',
   removed: 'Property was removed',
 };
 
 const messagesMap = {
-  updated: (keyNode, path) => makeMessageItem(keyNode.type, messageTextsMap[keyNode.type], path, keyNode.value, keyNode.preValue),
-  added: (keyNode, path) => makeMessageItem(keyNode.type, messageTextsMap[keyNode.type], path, keyNode.value),
-  removed: (keyNode, path) => makeMessageItem(keyNode.type, messageTextsMap[keyNode.type], path, keyNode.value),
-  parent: (keyNode, path, makeMessagesIter) => {
-    const messages = makeMessagesIter(keyNode.children, path);
-    return makeMessageItem(keyNode.type, messageTextsMap[keyNode.type], path, messages);
-    return makeMessagesIter(keyNode.children, path);
-  },
+  updated: (keyNode, path) => (
+    makeMessageItem(keyNode.type, textsMap[keyNode.type], path, keyNode.value, keyNode.prevValue)
+  ),
+  added: (keyNode, path) => (
+    makeMessageItem(keyNode.type, textsMap[keyNode.type], path, keyNode.value)
+  ),
+  removed: (keyNode, path) => (
+    makeMessageItem(keyNode.type, textsMap[keyNode.type], path, keyNode.value)
+  ),
+  parent: (keyNode, path, makeMessagesIter) => (
+    makeMessagesIter(keyNode.children, path)
+  ),
 };
 
 const makeMessages = (ast) => {
-  // console.log(ast);
   const makeMessagesIter = (currentAst, prevPath) => currentAst
     .reduce((acc, keyNode) => {
       const { type, name } = keyNode;
       const currentPath = prevPath === null ? name : `${prevPath}.${name}`;
+      if (type === 'same') {
+        return acc;
+      }
       return type === 'parent'
         ? [...acc, ...messagesMap[type](keyNode, currentPath, makeMessagesIter)]
-        // ? [...acc, ]
         : [...acc, messagesMap[type](keyNode, currentPath)];
     }, []);
 
   return makeMessagesIter(ast, null);
 };
 
-
 const makeReport = (messages, firstFilePath, secondFilePath) => {
-  return messages;
+  const report = {
+    firstFilePath,
+    secondFilePath,
+    messages,
+    updatedCount: messages.filter(({ actionId }) => actionId === 'updated').length,
+    addedCount: messages.filter(({ actionId }) => actionId === 'added').length,
+    removedCount: messages.filter(({ actionId }) => actionId === 'removed').length,
+  };
+
+  return report;
 };
 
 export default (ast, firstFilePath, secondFilePath) => {
   const messages = makeMessages(ast);
-
-  console.log(messages);
-
   const report = makeReport(messages, firstFilePath, secondFilePath);
 
   return JSON.stringify(report);
 };
-
-
-
-
-// => backup
-// const makeMessages = (ast) => {
-//   console.log(ast);
-//   const makeMessagesIter = (currentAst, prevPath) => currentAst
-//     .reduce((acc, keyNode) => {
-//       const { type, name } = keyNode;
-//       const currentPath = prevPath === null ? name : `${prevPath}.${name}`;
-//       return type === 'parent'
-//         ? [...acc, ...messagesMap[type](keyNode, currentPath, makeMessagesIter)]
-//         : [...acc, messagesMap[type](keyNode, currentPath)];
-//     }, []);
-
-//   return makeMessagesIter(ast, null);
-// };
