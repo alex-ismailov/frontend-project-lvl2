f37b5061d2d345018ce3678e7937d0eb5ecfb96d
smilenb <alexander.is.dev@gmail.com>
2020-12-24 16:14:51 +0300

    Refactor(__tests__/differ.test.js) use test.each instead of describe.each

--- __tests__/differ.test.js
+++ __tests__/differ.test.js
@@ -10,48 +10,57 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-let file1JsonPath;
-let file2JsonPath;
-let stylishDiffOfFile1AndFile2;
+const file1JsonPath = getFixturePath('file1.json');
+const file2JsonPath = getFixturePath('file2.json');
+const fileEmptyJsonPath = getFixturePath('fileEmpty.json');
+const file1YamlPath = getFixturePath('file1.yaml');
+const file2YamlPath = getFixturePath('file2.yaml');
+const fileEmptyYamlPath = getFixturePath('fileEmpty.yaml');
 
-beforeAll(() => {
-  file1JsonPath = getFixturePath('file1.json');
-  file2JsonPath = getFixturePath('file2.json');
-  stylishDiffOfFile1AndFile2 = readFile('stylishDiffOfFile1AndFile2.txt').trim();
-});
+const stylishDiffOfFile1AndFile2 = readFile('stylishDiffOfFile1AndFile2.txt').trim();
+const stylishDiffOfFile1AndFileEmpty = readFile('stylishDiffOfFile1AndFileEmpty.txt').trim();
+const stylishDiffOfEmptyFiles = readFile('stylishDiffOfEmptyFiles.txt').trim();
 
-describe.each([
-  [getFixturePath('file1.json'), getFixturePath('file2.json'), readFile('stylishDiffOfFile1AndFile2.txt')],
-  [getFixturePath('file1.json'), getFixturePath('file2.yaml'), readFile('stylishDiffOfFile1AndFile2.txt')],
-  [getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), readFile('stylishDiffOfFile1AndFile2.txt')],
-  [getFixturePath('file1.json'), getFixturePath('fileEmpty.yaml'), readFile('stylishDiffOfFile1AndFileEmpty.txt')],
-  [getFixturePath('fileEmpty.json'), getFixturePath('fileEmpty.yaml'), readFile('stylishDiffOfEmptyFiles.txt')],
-])('Test diff of %s %s', (filepath1, filepath2, expected) => {
-  test('Stylish output', () => {
+const plainDiffOfFile1AndFile2 = readFile('plainDiffOfFile1AndFile2.txt').trim();
+const plainDiffOfFile1AndFileEmpty = readFile('plainDiffOfFile1AndFileEmpty.txt').trim();
+const plainDiffOfEmptyFiles = readFile('plainDiffOfEmptyFiles.txt').trim();
+
+const jsonDiffOfFile1AndFile2 = readFile('jsonDiffOfFile1AndFile2.txt').trim();
+const jsonDiffOfFile1AndFileEmpty = readFile('jsonDiffOfFile1AndFileEmpty.txt').trim();
+const jsonDiffOfEmptyFiles = readFile('jsonDiffOfEmptyFiles.txt').trim();
+
+describe('Stylish output tests', () => {
+  test.each([
+    [file1JsonPath, file2JsonPath, stylishDiffOfFile1AndFile2],
+    [file1YamlPath, file2YamlPath, stylishDiffOfFile1AndFile2],
+    [file1JsonPath, file2YamlPath, stylishDiffOfFile1AndFile2],
+    [file1JsonPath, fileEmptyJsonPath, stylishDiffOfFile1AndFileEmpty],
+    [fileEmptyJsonPath, fileEmptyYamlPath, stylishDiffOfEmptyFiles],
+  ])('Test diff of %s %s', (filepath1, filepath2, expected) => {
     expect(differ(filepath1, filepath2)).toEqual(expected.trim());
   });
 });
 
-describe.each([
-  [getFixturePath('file1.json'), getFixturePath('file2.json'), readFile('plainDiffOfFile1AndFile2.txt')],
-  [getFixturePath('file1.json'), getFixturePath('file2.yaml'), readFile('plainDiffOfFile1AndFile2.txt')],
-  [getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), readFile('plainDiffOfFile1AndFile2.txt')],
-  [getFixturePath('file1.json'), getFixturePath('fileEmpty.yaml'), readFile('plainDiffOfFile1AndFileEmpty.txt')],
-  [getFixturePath('fileEmpty.json'), getFixturePath('fileEmpty.yaml'), readFile('plainDiffOfEmptyFiles.txt')],
-])('Test diff of %s %s', (filepath1, filepath2, expected) => {
-  test('Plain output', () => {
+describe('Plain output tests', () => {
+  test.each([
+    [file1JsonPath, file2JsonPath, plainDiffOfFile1AndFile2],
+    [file1YamlPath, file2YamlPath, plainDiffOfFile1AndFile2],
+    [file1JsonPath, file2YamlPath, plainDiffOfFile1AndFile2],
+    [file1JsonPath, fileEmptyJsonPath, plainDiffOfFile1AndFileEmpty],
+    [fileEmptyJsonPath, fileEmptyYamlPath, plainDiffOfEmptyFiles],
+  ])('Test diff of %s %s', (filepath1, filepath2, expected) => {
     expect(differ(filepath1, filepath2, 'plain')).toEqual(expected.trim());
   });
 });
 
-describe.each([
-  [getFixturePath('file1.json'), getFixturePath('file2.json'), readFile('jsonDiffOfFile1AndFile2.txt')],
-  [getFixturePath('file1.json'), getFixturePath('file2.yaml'), readFile('jsonDiffOfFile1AndFile2.txt')],
-  [getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), readFile('jsonDiffOfFile1AndFile2.txt')],
-  [getFixturePath('file1.json'), getFixturePath('fileEmpty.yaml'), readFile('jsonDiffOfFile1AndFileEmpty.txt')],
-  [getFixturePath('fileEmpty.json'), getFixturePath('fileEmpty.yaml'), readFile('jsonDiffOfEmptyFiles.txt')],
-])('Test diff of %s %s', (filepath1, filepath2, expected) => {
-  test('JSON output', () => {
+describe('JSON output tests', () => {
+  test.each([
+    [file1JsonPath, file2JsonPath, jsonDiffOfFile1AndFile2],
+    [file1YamlPath, file2YamlPath, jsonDiffOfFile1AndFile2],
+    [file1JsonPath, file2YamlPath, jsonDiffOfFile1AndFile2],
+    [file1JsonPath, fileEmptyJsonPath, jsonDiffOfFile1AndFileEmpty],
+    [fileEmptyJsonPath, fileEmptyYamlPath, jsonDiffOfEmptyFiles],
+  ])('Test diff of %s %s', (filepath1, filepath2, expected) => {
     expect(differ(filepath1, filepath2, 'json')).toEqual(expected.trim());
   });
 });
