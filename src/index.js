331b90bda706de542eef322de1e0ac25b9cddc8f
smilenb <alexander.is.dev@gmail.com>
2020-12-25 15:10:53 +0300

    Remove(src/index.js) getFormatter step

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { parseFile } from './utils.js';
 import getObjectsDiffAST from './differ.js';
-import getFormatter from './formatters/index.js';
+import format from './formatters/index.js';
 
 export default (file1Path, file2Path, formatStyle = 'stylish') => {
   const data1 = parseFile(file1Path);
   const data2 = parseFile(file2Path);
-
   const ast = getObjectsDiffAST(data1, data2);
-  const format = getFormatter(formatStyle);
 
-  return format(ast);
+  return format(ast, formatStyle);
 };
