cd84cc04433d60d782acf6bd3ec08c51913df383
smlnx <alexander.is.dev@gmail.com>
2020-12-27 01:30:56 +0300

    Refactor(src/parsers.js) the parsers module should return a function, not an object. The function accepts data and data format.

--- src/parsers.js
+++ src/parsers.js
@@ -1,6 +1,13 @@
 import yaml from 'js-yaml';
 
-export default {
-  json: (data) => JSON.parse(data),
-  yaml: (data) => yaml.safeLoad(data) ?? {},
+export default (data, extension) => {
+  switch (extension) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yaml':
+      return yaml.safeLoad(data) ?? {};
+
+    default:
+      throw new Error(`Unknown file extension: ! ---> ${extension} <--- !`);
+  }
 };
