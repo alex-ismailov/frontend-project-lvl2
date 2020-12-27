d00475ebf9930a7e24879561dc24aa6cbee804dd
smlnx <alexander.is.dev@gmail.com>
2020-12-28 02:25:07 +0300

    Add(src/index.js) parseFile function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import { parseFile } from './utils.js';
+import fs from 'fs';
+import path from 'path';
 import buildDiffTree from './differ.js';
 import format from './formatters/index.js';
+import parse from './parsers.js';
+
+const parseFile = (filePath) => {
+  const fullPath = path.resolve(process.cwd(), filePath);
+  const rawData = fs.readFileSync(filePath, 'utf-8').toString();
+  const fileExtension = path.extname(fullPath).slice(1);
+
+  return parse(rawData, fileExtension);
+};
 
 export default (filepath1, filepath2, formatStyle = 'stylish') => {
   const fileContent1 = parseFile(filepath1);
