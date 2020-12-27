5513427e13f713509f22cb169d18b64f06e8d58a
smlnx <alexander.is.dev@gmail.com>
2020-12-27 17:41:19 +0300

    Remove(src/utils.js) getKeysUnion function

--- src/utils.js
+++ src/utils.js
@@ -8,15 +8,6 @@ export const isObject = (value) => (
     : value.constructor.name === 'Object'
 );
 
-export const getKeysUnion = (obj1, obj2) => {
-  const obj1Keys = Object.keys(obj1);
-  const obj2Keys = Object.keys(obj2);
-  const allKeys = [...obj1Keys, ...obj2Keys];
-  const allKeysSet = new Set(allKeys);
-
-  return Array.from(allKeysSet);
-};
-
 export const parseFile = (filePath) => {
   const fullPath = path.resolve(process.cwd(), filePath);
   const rawData = fs.readFileSync(filePath, 'utf-8').toString();
