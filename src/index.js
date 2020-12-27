3d21210c9199fb122854114c8efdec8ea49b203a
smlnx <alexander.is.dev@gmail.com>
2020-12-27 23:46:10 +0300

    Rename(src/index.js) params and consts

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import { parseFile } from './utils.js';
 import buildDiffTree from './differ.js';
 import format from './formatters/index.js';
 
-export default (file1Path, file2Path, formatStyle = 'stylish') => {
-  const data1 = parseFile(file1Path);
-  const data2 = parseFile(file2Path);
-  const diffTree = buildDiffTree(data1, data2);
+export default (filepath1, filepath2, formatStyle = 'stylish') => {
+  const fileContent1 = parseFile(filepath1);
+  const fileContent2 = parseFile(filepath2);
+  const diffTree = buildDiffTree(fileContent1, fileContent2);
 
   return format(diffTree, formatStyle);
 };
