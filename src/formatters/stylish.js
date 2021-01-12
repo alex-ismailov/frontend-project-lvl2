661248d7af6608b43f3926ae9adef558e6bc39ba
smlnx <alexander.is.dev@gmail.com>
2021-01-13 00:17:06 +0300

    Refactor(src/formatters/stylish.js) remove braces and shorten nested, root cases

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -31,27 +31,22 @@ const format = (diffNode, depth) => {
     key, type, value, previousValue, currentValue, children,
   } = diffNode;
   const currentTab = type !== 'root' ? getCurrentTab(depth) : tab.repeat(0);
-
   switch (type) {
     case 'added':
     case 'removed':
-    case 'unchanged': {
+    case 'unchanged':
       return `${currentTab}${actionPrefixMap[type]}${key}: ${stringifyValue(value, depth, format)}`;
-    }
-    case 'updated': {
+    case 'updated':
       return [
         `${currentTab}${actionPrefixMap.removed}${key}: ${stringifyValue(previousValue, depth, format)}`,
         `${currentTab}${actionPrefixMap.added}${key}: ${stringifyValue(currentValue, depth, format)}`,
       ];
-    }
     case 'nested': {
-      const rows = children.flatMap((node) => format(node, depth + 1));
-      const row = rows.join('\n');
+      const row = children.flatMap((node) => format(node, depth + 1)).join('\n');
       return `${currentTab}${actionPrefixMap[type]}${key}: {\n${row}\n${currentTab}${actionPrefixMap[type]}}`;
     }
     case 'root': {
-      const rows = diffNode.children.map((node) => format(node, depth + 1));
-      const row = rows.join('\n');
+      const row = diffNode.children.map((node) => format(node, depth + 1)).join('\n');
       return `{\n${row}\n}`;
     }
     default:
