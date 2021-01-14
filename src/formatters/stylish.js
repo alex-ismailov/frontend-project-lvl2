0b78a0e95a4ed0b6ae4c49deb4b82f1a86911ce6
smlnx <alexander.is.dev@gmail.com>
2021-01-14 15:50:18 +0300

    Refactor(src/formatters/) stylish.js
    Remove actionPrefixMap just use prefixes where necessary.
    Use getIndent where necessary.
    Use getIndent (1) before the closing brace instead of actionPrefixMap.unchanged.
    The error message must be started with a capital letter.

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,12 +1,6 @@
 import isPlainObject from 'lodash/isPlainObject.js';
 
 const tab = ' '.repeat(2);
-const diffPrefixesMap = {
-  unchanged: ' '.repeat(2),
-  nested: ' '.repeat(2),
-  added: '+ ',
-  removed: '- ',
-};
 
 const getIndent = (depth) => tab.repeat(2 * depth - 1);
 
@@ -20,39 +14,37 @@ const stringifyValue = (value, depth, format) => {
       const unchangedDiffNode = { key, type: 'unchanged', value: value[key] };
       return format(unchangedDiffNode, depth + 1);
     });
-  const row = rows.join('\n');
-  const currentTab = getIndent(depth);
+  const currentIndent = getIndent(depth);
 
-  return `{\n${row}\n${currentTab}${diffPrefixesMap.unchanged}}`;
+  return `{\n${rows.join('\n')}\n${currentIndent}${getIndent(1)}}`;
 };
 
 const format = (diffNode, depth) => {
   const {
     key, type, value, previousValue, currentValue, children,
   } = diffNode;
-  const currentTab = type !== 'root' ? getIndent(depth) : tab.repeat(0);
   switch (type) {
     case 'added':
+      return `${getIndent(depth)}+ ${key}: ${stringifyValue(value, depth, format)}`;
     case 'removed':
+      return `${getIndent(depth)}- ${key}: ${stringifyValue(value, depth, format)}`;
     case 'unchanged':
-      return `${currentTab}${diffPrefixesMap[type]}${key}: ${stringifyValue(value, depth, format)}`;
+      return `${getIndent(depth)}${' '.repeat(2)}${key}: ${stringifyValue(value, depth, format)}`;
     case 'updated':
       return [
-        `${currentTab}${diffPrefixesMap.removed}${key}: ${stringifyValue(previousValue, depth, format)}`,
-        `${currentTab}${diffPrefixesMap.added}${key}: ${stringifyValue(currentValue, depth, format)}`,
+        `${getIndent(depth)}- ${key}: ${stringifyValue(previousValue, depth, format)}`,
+        `${getIndent(depth)}+ ${key}: ${stringifyValue(currentValue, depth, format)}`,
       ];
     case 'nested': {
       const rows = children.flatMap((node) => format(node, depth + 1));
-      const row = rows.join('\n');
-      return `${currentTab}${diffPrefixesMap[type]}${key}: {\n${row}\n${currentTab}${diffPrefixesMap[type]}}`;
+      return `${getIndent(depth)}${' '.repeat(2)}${key}: {\n${rows.join('\n')}\n${getIndent(depth)}${getIndent(1)}}`;
     }
     case 'root': {
       const rows = diffNode.children.map((node) => format(node, depth + 1));
-      const row = rows.join('\n');
-      return `{\n${row}\n}`;
+      return `{\n${rows.join('\n')}\n}`;
     }
     default:
-      throw new Error(`unknown diffNode type: ${type}`);
+      throw new Error(`Unknown diffNode type: ${type}`);
   }
 };
 
