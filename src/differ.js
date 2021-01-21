b4d814b45dbaf9ebeaf8bdf24c25f4821aef171a
smlnx <alexander.is.dev@gmail.com>
2021-01-22 01:58:31 +0300

    Refactor(src/differ.js) use _ when importing lodash

--- src/differ.js
+++ src/differ.js
@@ -1,28 +1,24 @@
 import _ from 'lodash';
 
-const {
-  has, union, sortBy, isPlainObject, isEqual,
-} = _;
-
 const getDiffTreeChildren = (obj1, obj2) => {
-  const keysUnion = union(Object.keys(obj1), Object.keys(obj2));
-  const sortedKeysUnion = sortBy(keysUnion);
+  const keysUnion = _.union(Object.keys(obj1), Object.keys(obj2));
+  const sortedKeysUnion = _.sortBy(keysUnion);
 
   return sortedKeysUnion
     .map((key) => {
-      if (isPlainObject(obj1[key]) && isPlainObject(obj2[key])) {
+      if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
         return { key, type: 'nested', children: getDiffTreeChildren(obj1[key], obj2[key]) };
       }
-      if (isEqual(obj1[key], obj2[key])) {
+      if (_.isEqual(obj1[key], obj2[key])) {
         return { key, type: 'unchanged', value: obj1[key] };
       }
-      if (has(obj1, key) && has(obj2, key)) {
+      if (_.has(obj1, key) && _.has(obj2, key)) {
         return {
           key, type: 'updated', currentValue: obj2[key], previousValue: obj1[key],
         };
       }
 
-      return has(obj1, key)
+      return _.has(obj1, key)
         ? { key, type: 'removed', value: obj1[key] }
         : { key, type: 'added', value: obj2[key] };
     });
