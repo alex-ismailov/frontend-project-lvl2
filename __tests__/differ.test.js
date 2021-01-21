ac494b3d91dd0d482a1d2c97c4aad78bf3f7d76c
smlnx <alexander.is.dev@gmail.com>
2021-01-22 01:18:07 +0300

    Remove(__tests__/differ.test.js) reduce the number of tests in main flow group

--- __tests__/differ.test.js
+++ __tests__/differ.test.js
@@ -11,25 +11,18 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 const file1JsonPath = getFixturePath('file1.json');
 const file2JsonPath = getFixturePath('file2.json');
-const file1JsonRelPath = '__fixtures__/file1.json';
-const file2JsonRelPath = '__fixtures__/file2.json';
 const fileEmptyJsonPath = getFixturePath('fileEmpty.json');
-const file1YamlPath = getFixturePath('file1.yaml');
+
 const file2YamlPath = getFixturePath('file2.yaml');
-const file1YmlPath = getFixturePath('file1.yml');
 const file2YmlPath = getFixturePath('file2.yml');
-const fileEmptyYamlPath = getFixturePath('fileEmpty.yaml');
 
 const stylishDiffOfFile1AndFile2 = readFile('stylishDiffOfFile1AndFile2.txt');
-const stylishDiffOfFile1AndFileEmpty = readFile('stylishDiffOfFile1AndFileEmpty.txt');
 const stylishDiffOfEmptyFiles = readFile('stylishDiffOfEmptyFiles.txt');
 
 const plainDiffOfFile1AndFile2 = readFile('plainDiffOfFile1AndFile2.txt');
-const plainDiffOfFile1AndFileEmpty = readFile('plainDiffOfFile1AndFileEmpty.txt');
 const plainDiffOfEmptyFiles = readFile('plainDiffOfEmptyFiles.txt');
 
 const jsonDiffOfFile1AndFile2 = readFile('jsonDiffOfFile1AndFile2.json');
-const jsonDiffOfFile1AndFileEmpty = readFile('jsonDiffOfFile1AndFileEmpty.json');
 const jsonDiffOfEmptyFiles = readFile('jsonDiffOfEmptyFiles.json');
 
 const file1TxtPath = getFixturePath('file1.txt');
@@ -38,22 +31,13 @@ describe('Main flow', () => {
   test.each`
         filepath1        |     filepath2        | outputStyle  |          expected
     ${file1JsonPath}     | ${file2JsonPath}     | ${undefined} | ${stylishDiffOfFile1AndFile2}
-    ${file1JsonRelPath}  | ${file2JsonRelPath}  | ${'stylish'} | ${stylishDiffOfFile1AndFile2}
-    ${file1YamlPath}     | ${file2YamlPath}     | ${'stylish'} | ${stylishDiffOfFile1AndFile2}
     ${file1JsonPath}     | ${file2YamlPath}     | ${'stylish'} | ${stylishDiffOfFile1AndFile2}
-    ${file1YmlPath}      | ${file2YmlPath}      | ${'stylish'} | ${stylishDiffOfFile1AndFile2}
-    ${file1JsonPath}     | ${fileEmptyJsonPath} | ${'stylish'} | ${stylishDiffOfFile1AndFileEmpty}
-    ${fileEmptyJsonPath} | ${fileEmptyYamlPath} | ${'stylish'} | ${stylishDiffOfEmptyFiles}
+    ${file1JsonPath}     | ${file2YmlPath}      | ${'stylish'} | ${stylishDiffOfFile1AndFile2}
+    ${fileEmptyJsonPath} | ${fileEmptyJsonPath} | ${'stylish'} | ${stylishDiffOfEmptyFiles}
     ${file1JsonPath}     | ${file2JsonPath}     | ${'plain'}   | ${plainDiffOfFile1AndFile2}
-    ${file1YamlPath}     | ${file2YamlPath}     | ${'plain'}   | ${plainDiffOfFile1AndFile2}
-    ${file1JsonPath}     | ${file2YamlPath}     | ${'plain'}   | ${plainDiffOfFile1AndFile2}
-    ${file1JsonPath}     | ${fileEmptyJsonPath} | ${'plain'}   | ${plainDiffOfFile1AndFileEmpty}
-    ${fileEmptyJsonPath} | ${fileEmptyYamlPath} | ${'plain'}   | ${plainDiffOfEmptyFiles}
+    ${fileEmptyJsonPath} | ${fileEmptyJsonPath} | ${'plain'}   | ${plainDiffOfEmptyFiles}
     ${file1JsonPath}     | ${file2JsonPath}     | ${'json'}    | ${jsonDiffOfFile1AndFile2}
-    ${file1YamlPath}     | ${file2YamlPath}     | ${'json'}    | ${jsonDiffOfFile1AndFile2}
-    ${file1JsonPath}     | ${file2YamlPath}     | ${'json'}    | ${jsonDiffOfFile1AndFile2}
-    ${file1JsonPath}     | ${fileEmptyJsonPath} | ${'json'}    | ${jsonDiffOfFile1AndFileEmpty}
-    ${fileEmptyJsonPath} | ${fileEmptyYamlPath} | ${'json'}    | ${jsonDiffOfEmptyFiles}
+    ${fileEmptyJsonPath} | ${fileEmptyJsonPath} | ${'json'}    | ${jsonDiffOfEmptyFiles}
   `('Output style: $outputStyle; Test diff of: $filepath1 and $filepath2', ({
     filepath1, filepath2, outputStyle, expected,
   }) => {
