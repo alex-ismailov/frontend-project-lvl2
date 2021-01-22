84ba5f8b5fc8072980a3a620931c40333ec638e2
smilenb <alexander.is.dev@gmail.com>
2021-01-22 16:14:18 +0300

    Refactor(src/differ.js) if cases in getDiffTreeChildren function

--- src/differ.js
+++ src/differ.js
@@ -6,21 +6,22 @@ const getDiffTreeChildren = (obj1, obj2) => {
 
   return sortedKeysUnion
     .map((key) => {
-      if (_.has(obj1, key) && !_.has(obj2, key)) {
-        return { key, type: 'removed', value: obj1[key] };
-      }
-      if (!_.has(obj1, key) && _.has(obj2, key)) {
+      if (!_.has(obj1, key)) {
         return { key, type: 'added', value: obj2[key] };
+      }
+      if (!_.has(obj2, key)) {
+        return { key, type: 'removed', value: obj1[key] };
       }
       if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
         return { key, type: 'nested', children: getDiffTreeChildren(obj1[key], obj2[key]) };
       }
+      if (_.isEqual(obj1[key], obj2[key])) {
+        return { key, type: 'unchanged', value: obj1[key] };
+      }
 
-      return _.isEqual(obj1[key], obj2[key])
-        ? { key, type: 'unchanged', value: obj1[key] }
-        : {
-          key, type: 'updated', currentValue: obj2[key], previousValue: obj1[key],
-        };
+      return {
+        key, type: 'updated', currentValue: obj2[key], previousValue: obj1[key],
+      };
     });
 };
 
